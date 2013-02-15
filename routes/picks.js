392ff3543ac222e1f6f5a19bb9fc2f09a0770c8f
Clemens Park <clemens.park@gmail.com>
2013-02-15 16:11:17 -0500

    populates the picks for the edit page

--- routes/picks.js
+++ routes/picks.js
@@ -1,10 +1,15 @@
-exports.edit = function(req, res){
-  //DB call to get the user
-  var user = {name: 'Jason'}
-  res.send("Editing picks for user" + user.name);
-  res.render("edit_picks", {
-    title: "Make your final picks",
-    candidates: this.candidates(),
-    picks: {first: "Lindsay", second: "Catherine", third: "Desiree", fourth: "Ashlee"}
-  })
+module.exports = {
+  edit: function(req, res){
+    //DB call to get the user
+    var user = {name: 'Jason'}
+    res.render("edit_picks", {
+      title: "Make your final picks",
+      candidates: this.candidates(),
+      picks: {first: "Lindsay", second: "Catherine", third: "Desiree", fourth: "Ashlee"}
+    })
+  },
+
+  candidates: function(){
+    return [{name: 'Catherine'}, {name: 'Ashlee'}, {name:'Desiree'}, {name:'Lindsay'}]
+  }
 };
