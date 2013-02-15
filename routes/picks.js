10709c68ed67610983d7f95868c91d1bf48696bf
Clemens Park <clemens.park@gmail.com>
2013-02-15 15:27:43 -0500

    replaces hogan.js with handlebars.js

--- routes/picks.js
+++ routes/picks.js
@@ -2,4 +2,9 @@ exports.edit = function(req, res){
   //DB call to get the user
   var user = {name: 'Jason'}
   res.send("Editing picks for user" + user.name);
+  res.render("edit_picks", {
+    title: "Make your final picks",
+    candidates: this.candidates(),
+    picks: {first: "Lindsay", second: "Catherine", third: "Desiree", fourth: "Ashlee"}
+  })
 };
